d6ae9b2e6b21d148f9701dbb4c9f0014ce87aca3
Husan Eshonqulov <husanswe@gmail.com>
2026-03-22 15:15:55 +0500

    setup session

--- src/bot.ts
+++ src/bot.ts
@@ -1,7 +1,16 @@
-import { Bot } from "grammy";
+import { Bot, Context, session, SessionFlavor } from "grammy";
 
+import { redisStorage } from "./db.js";
 import { env } from "./env.js";
 
-export const bot = new Bot(env.BOT_TOKEN);
+interface SessionData {
+  __language_code: string;
+}
+
+type BotContext = Context & SessionFlavor<SessionData>;
+
+export const bot = new Bot<BotContext>(env.BOT_TOKEN);
+
+bot.use(session({ initial: () => ({}), storage: redisStorage }));
 
 bot.command("start", (ctx) => ctx.reply("Welcome!"));
--- src/env.ts
+++ src/env.ts
@@ -1,7 +1,8 @@
 import zod from "zod";
 
 const envSchema = zod.object({
-  BOT_TOKEN: zod.string()
+  BOT_TOKEN: zod.string(),
+  REDIS_URL: zod.url().startsWith("redis://")
 });
 
 export const env = envSchema.parse(process.env);
